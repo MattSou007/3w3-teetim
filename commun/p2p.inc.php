d7a349ae544760b20a393406e9d0198a4c50dc50
Matheo Sousa <e2592801@cmaisonneuve.qc.ca>
2026-09-02 11:08:17 -0400

    debut php

---
+++ commun/p2p.inc.php
@@ -0,0 +1,33 @@
+        <footer>
+            <h2>teeTIM</h2>
+            <div class="contenu">
+                <section class="achats">
+                    <h3>Vos achats</h3>
+                    <nav>
+                        <a href="faq.html" class="faq">Foire aux questions</a>
+                        <a href="livraison.html" class="livraison">Livraison de votre colis</a>
+                        <a href="conditions.html" class="conditions">Conditions de vente</a>
+                        <a href="confidentialite.html" class="confidentialite">Politique de confidentialité</a>
+                    </nav>
+                </section>
+                <section class="apropos">
+                    <h3>À propos de teeTIM</h3>
+                    <nav>
+                        <a href="compagnie.html" class="faq">La compagnie</a>
+                        <a href="equipe.html" class="livraison">L'équipe</a>
+                        <a href="emploi.html" class="conditions">Emplois</a>
+                    </nav>
+                </section>
+                <section class="coordonnees">
+                    <h3>Nous joindre</h3>
+                    <nav>
+                        <span>Sans frais : <b>1 866 888 6666</b></span>
+                        <span>Courriel : aide@teetim.ca</span>
+                    </nav>
+                </section>
+            </div>
+            <p class="da">&copy; Tous droits réservés, teeTIM 2023-<?php echo date ('Y'); ?></p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ teeshirts.php
@@ -0,0 +1,16 @@
+<?php
+    // Inclure le fichier commun contenant le haut du code des écrans de UI
+    include_once("commun/header.inc.php");
+?>
+<main class="page-teeshirts">
+    <article class="amorce">
+        <h1>Nos teeshirts</h1>
+    </article>
+    <article class="principal">
+        À venir...
+    </article>
+</main>
+<?php
+    // Inclure le fichier commun contenant le bas du code des écrans de UI
+    include_once("commun/p2p.inc.php");
+?>
